c526d65ac40a2289fb27de029054c329a4574a50
Holger Benl <hbenl@evandor.de>
2019-06-01 15:23:31 +0200

    update and cleanup

--- docker-launcher.js
+++ docker-launcher.js
@@ -1,10 +1,6 @@
 const { spawn } = require('child_process');
 const { inspect } = require('util');
-const { mochaWorker, createConnection, writeMessage, readMessages } = require('vscode-test-adapter-remoting-util');
-
-// TODO:
-// always handle the exit option in the worker process
-// error recovery: call docker rm -f when we receive a signal
+const { mochaWorker, convertPath, createConnection, writeMessage, readMessages } = require('vscode-test-adapter-remoting-util');
 
 const localWorkspace = __dirname;
 const remoteHome = '/home/node';
@@ -12,27 +8,15 @@ const remoteWorker = remoteHome + '/worker.js';
 const remoteWorkspace = remoteHome + '/workspace';
 const port = 8123;
 
-function convertPaths(srcPath, dstPath) {
-	return function(path) {
-		if (path.startsWith(srcPath)) {
-			return dstPath + path.substring(srcPath.length)
-		} else {
-			return path;
-		}
-	}
-}
-const localToRemote = convertPaths(localWorkspace, remoteWorkspace);
-const remoteToLocal = convertPaths(remoteWorkspace, localWorkspace);
+const log = msg => process.send(msg);
+const localToRemotePath = path => convertPath(path, localWorkspace, remoteWorkspace);
+const remoteToLocalPath = path => convertPath(path, remoteWorkspace, localWorkspace);
 
-process.once('message', workerArgsJson => {
+process.once('message', async origWorkerArgs => {
 
-	const origWorkerArgs = JSON.parse(workerArgsJson);
-	const localWorker = origWorkerArgs.workerScript;
+	log('Received workerArgs');
 
-	process.send('Received workerArgs');
-
-	const workerArgs = mochaWorker.convertWorkerArgs(origWorkerArgs, localToRemote);
-	workerArgs.mochaPath = localToRemote(origWorkerArgs.mochaPath);
+	const workerArgs = mochaWorker.convertWorkerArgs(origWorkerArgs, localToRemotePath);
 
 	let nodeDebugArgs = [];
 	let dockerDebugArgs = [];
@@ -43,15 +27,14 @@ process.once('message', workerArgsJson => {
 		rejectClosedSocket = 1500;
 	}
 
-	process.send('Starting worker process');
-
+	log('Starting worker process');
 	const childProcess = spawn(
 		'docker',
 		[
-			'run', '--rm', '-i',
-			'-v', `${localWorker}:${remoteWorker}`,
+			'run', '--rm',
+			'-v', `${origWorkerArgs.workerScript}:${remoteWorker}`,
 			'-v', `${localWorkspace}:${remoteWorkspace}`,
-			'-w', localToRemote(process.cwd()),
+			'-w', localToRemotePath(process.cwd()),
 			'-p', `${port}:${port}`,
 			...dockerDebugArgs,
 			'node:current-alpine',
@@ -62,30 +45,27 @@ process.once('message', workerArgsJson => {
 		{ stdio: 'inherit' }
 	);
 
-	childProcess.on('error', err => process.send(`Error from worker process: ${inspect(err)}`));
+	childProcess.on('error', err => log(`Error from docker: ${inspect(err)}`));
 	childProcess.on('exit', (code, signal) => {
-		process.send(`Worker process exited with code ${code} and signal ${signal}`);
+		log(`The docker process exited with code ${code} and signal ${signal}.`);
 		if ((workerArgs.action === 'loadTests') && (code || signal)) {
-			process.send({ type: 'finished', errorMessage: `The worker process finished with code ${code} and signal ${signal}.\nThe diagnostic log may contain more information, enable it with the "mochaExplorer.logpanel" or "mochaExplorer.logfile" settings.` });
+			process.send({ type: 'finished', errorMessage: `The docker process exited with code ${code} and signal ${signal}.\nThe diagnostic log may contain more information, enable it with the "mochaExplorer.logpanel" or "mochaExplorer.logfile" settings.` });
 		}
 	});
 
-	process.send('Connecting to worker process');
+	log('Connecting to worker process');
+	const socket = await createConnection(port, { rejectClosedSocket });
 
-	createConnection(port, { rejectClosedSocket }).then(socket => {
+	log('Sending workerArgs to worker process');
+	await writeMessage(socket, workerArgs);
 
-		process.send('Connected');
+	log('Finished initialising worker');
 
-		writeMessage(socket, workerArgs);
-
-		process.send('Sent workerArgs to worker process');
-
-		readMessages(socket, msg => {
-			if (workerArgs.action === 'loadTests') {
-				process.send(mochaWorker.convertTestLoadMessage(msg, remoteToLocal));
-			} else {
-				process.send(mochaWorker.convertTestRunMessage(msg, remoteToLocal));
-			}
-		});
+	readMessages(socket, msg => {
+		if (workerArgs.action === 'loadTests') {
+			process.send(mochaWorker.convertTestLoadMessage(msg, remoteToLocalPath));
+		} else {
+			process.send(mochaWorker.convertTestRunMessage(msg, remoteToLocalPath));
+		}
 	});
 });
